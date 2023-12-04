d4120ad16379dc4b157fb9001ec5f6f5ffea99dc
lylelow <lyleddawson26@gmail.com>
2023-12-04 18:38:02 +0300

    Guessing game is added

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+import random
+
 print("Hello, Stan.")
 for _ in range(3):
     print("I love you!")
+
+
+def guess_number():
+    number = random.randint(1, 100)
+    attempts = 0
+    while True:
+        guess = int(input("Guess a number between 1 and 100: "))
+        attempts += 1
+        if guess < number:
+            print("Too low! Try again.")
+        elif guess > number:
+            print("Too high! Try again.")
+        else:
+            print(f"Congratulations! You guessed the number in {attempts} attempts.")
+            break
+
+
+guess_number()
